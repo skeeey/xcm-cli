99eb1629e85c049dcae985920d1eead8c13399f4
Wei Liu <liuweixa@redhat.com>
2023-03-14 15:55:44 +0800

    use new controlplane
    Signed-off-by: Wei Liu <liuweixa@redhat.com>

--- contrib/clean.sh
+++ contrib/clean.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
 echo ">>> clean the spoke ..."
-# kubectl delete klusterlets --all
-# sleep 10
-# kubectl delete ns open-cluster-management-managed-serviceaccount --ignore-not-found
-# kubectl delete ns open-cluster-management --ignore-not-found
-# kubectl get ns | grep cluster | grep -v multicluster-controlplane | awk '{print $1}' | xargs kubectl delete ns
+kubectl delete ns multicluster-controlplane-agent --ignore-not-found
 
 echo ">>> clean the control plane ..."
-# kubectl --kubeconfig ${CONTROLPLANE_KUBECONFIG} delete managedcluster --all
-
 kubectl -n multicluster-controlplane delete deploy --all
 kubectl -n multicluster-controlplane delete secrets --all
